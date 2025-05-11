104240173a8e143e82e450ea68798cd4f72a1161
Woohyun Jo <bluewing1004@gmail.com>
2025-05-11 16:25:11 +0900

    [Design] 로그인 버튼 디자인

--- FLOV/Feature/00_Tab/View/ContentView.swift
+++ FLOV/Feature/00_Tab/View/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                print("카카오로그인탭")
+            } label: {
+                Image(.kakaoLogin)
+                    .resizable()
+                    .frame(width : UIScreen.main.bounds.width * 0.9, height: 56)
+                    .clipShape (
+                        RoundedRectangle(cornerRadius: 8)
+                    )
+            }
+            
+            ZStack {
+                SignInWithAppleButton { request in
+                    print(request)
+                    print("찐애플로그인탭")
+                } onCompletion: { result in
+                    print(result)
+                }
+                .frame(width : UIScreen.main.bounds.width * 0.9, height:54)
+                .clipShape (
+                    RoundedRectangle(cornerRadius: 8)
+                )
+                
+                Button {
+                    print("커스텀애플로그인탭")
+                } label: {
+                    Image(.appleLogin)
+                        .resizable()
+                        .frame(width : UIScreen.main.bounds.width * 0.9, height: 56)
+                        .clipShape (
+                            RoundedRectangle(cornerRadius: 8)
+                        )
+                }
+                .allowsHitTesting(false)
+            }
         }
-        .padding()
     }
 }
 
