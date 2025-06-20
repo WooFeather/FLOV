200dbb93a9066edcfb6fdcc3ccc8a61aca508c8a
Woohyun Jo <bluewing1004@gmail.com>
2025-06-20 23:55:09 +0900

    [Setting] PostView 기본 세팅

--- FLOV/Feature/03_Post/View/PostView.swift
+++ FLOV/Feature/03_Post/View/PostView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct PostView: View {
+    @EnvironmentObject var pathModel: PathModel
+    
     var body: some View {
-        Text("PostView")
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Text("PostView")
+            }
+        }
+        .asNavigationToolbar()
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                toolbarTitle()
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                writeButton()
+            }
+        }
     }
 }
 
-#Preview {
-    PostView()
+// MARK: - HeaderView
+private extension PostView {
+    func headerView() -> some View {
+        VStack {
+            Text("액티비티 포스트")
+            Text("거리 조절뷰")
+        }
+    }
+}
+
+// MARK: - PostListView
+private extension PostView {
+    func postListView() -> some View {
+        VStack {
+            Text("PostListView")
+        }
+    }
+}
+
+// MARK: - Toolbar
+private extension PostView {
+    func toolbarTitle() -> some View {
+        Text("POST")
+            .foregroundStyle(.colDeep)
+            .font(.Caption.caption0)
+            .lineLimit(1)
+    }
+    
+    func writeButton() -> some View {
+        Image(.icnWrite)
+            .resizable()
+            .frame(width: 32, height: 32)
+            .asButton {
+                pathModel.presentFullScreenCover(.postWrite)
+            }
+    }
 }
